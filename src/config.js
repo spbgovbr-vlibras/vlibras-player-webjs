6e2f3da457f1d85b32f73ac934e2c58b570a0df8
Suanny <suanny_fabyne@hotmail.com>
2019-05-16 15:16:44 -0300

    new routes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-exports.translatorUrl = 'http://150.165.204.184:8082/translate';
-exports.dictionaryUrl = 'http://150.165.204.184:8083/3_3_1/WEBGL/'; // Must have "/" in the end
-
+exports.translatorUrl = 'http://104.211.3.220/translate';
+exports.dictionaryUrl = 'http://52.179.96.27/3_3_1/WEBGL/'; // Must have "/" in the end
