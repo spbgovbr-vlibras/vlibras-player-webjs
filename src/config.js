bf001b6ad7f66fd1ee7bb1da8ab5332f231a8d55
Suanny <suanny_fabyne@hotmail.com>
2019-06-11 15:44:25 -0300

    new dns

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
-exports.translatorUrl = 'http://104.211.3.220/translate';
-exports.dictionaryUrl = 'http://52.179.96.27/3_3_1/WEBGL/'; // Must have "/" in the end
+// exports.translatorUrl = 'https://40.117.191.123/translate';
+// exports.dictionaryUrl = 'https://40.117.169.136/3_3_1/WEBGL/'; // Must have "/" in the end
+
+exports.translatorUrl = 'https://traducao2-dth.vlibras.gov.br/translate';
+exports.dictionaryUrl = 'https://dicionario2-dth.vlibras.gov.br/3_3_1/WEBGL/';
