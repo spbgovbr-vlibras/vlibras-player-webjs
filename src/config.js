57b8ae383260fd2595de0436dfa76d8ff3366517
Suanny <suanny_fabyne@hotmail.com>
2019-07-10 01:00:17 -0300

    new dns

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-exports.translatorUrl = 'https://traducao2-dth.vlibras.gov.br/translate';
-exports.dictionaryUrl = 'https://dicionario2-dth.vlibras.gov.br/2018.3.0/WEBGL/';
+exports.translatorUrl = 'https://traducao2.vlibras.gov.br/translate';
+exports.dictionaryUrl = 'https://dicionario2.vlibras.gov.br/2018.3.0/WEBGL/';
