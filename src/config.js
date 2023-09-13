e85767129b95bb7cd8048489073c3a2137d5bf03
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-09-13 20:27:59 -0300

    refactor: dth urls

--- src/config.js
+++ src/config.js
@@ -1,7 +1,2 @@
-// prod urls
-// exports.translatorUrl = 'https://traducao2.vlibras.gov.br/translate';
-// exports.dictionaryUrl = 'https://dicionario2.vlibras.gov.br/2018.3.1/WEBGL/';
-
-// homolog urls
 exports.translatorUrl = 'https://traducao2-dth.vlibras.gov.br/dl/translate';
 exports.dictionaryUrl = 'https://dicionario2-dth.vlibras.gov.br/2018.3.1/WEBGL/';
